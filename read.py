7bba1c7375171b41cddb03d1581ca326ef644fac
*** <zhuh813@gmail.com>
2019-04-27 22:27:12 +0800

    calculate aver len reviews

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 data = [] #建立空清單叫data
 count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f: #每次讀取(讀f)都是讀取一行 另外令一個變數叫作 "line"
 		data.append(line) #把讀取的line存入到list裡面
@@ -9,10 +10,17 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-#print(len(data)) #印出整個清單的長度
+print('檔案讀取完了, 總共有', len(data),'筆資料') #印出整個清單的長度
 
-print(data[0])#印出第一比資料
-print('-------------------------------------')
-print(data[1])
+#以下算出留言平均長度
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('留言平均長度',sum_len / len(data),)
+	
+
+
+
 
 
